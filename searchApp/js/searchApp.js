091e26f2eb0bacf4e1c69e986c9f4a97edd69b19
pekiZG <petar.slat@gmail.com>
2015-11-06 20:37:59 +0100

    Quickly scaffolded initial version of an app
    Added .js that actualy does some stuff (autocomplete and search query)

---
+++ searchApp/js/searchApp.js
@@ -0,0 +1,21 @@
+$(".form-control").keypress(function (event) {
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if (keycode == '13') {
+        $("tbody").empty();
+
+        $.get("http://tvz.com.hr/api/search/", { board: $(".form-control").val() }, function (data) {
+
+            $.each(data, function (index, attachment) {
+                $("tbody").append("<tr><td scope='row'>" + index + "</td><td><a href='http://tvz.com.hr/api/attachments/?id=" + attachment.ID_ATTACH + "'>" + attachment.filename + "</a></td></tr>");
+            });
+
+        }, "json");
+    }
+});
+
+var TVZBoardsAPI = "http://tvz.com.hr/api/boards/";
+$.getJSON(TVZBoardsAPI, function (data) {
+    $(".form-control").autocomplete({
+        source: data
+    });
+});
