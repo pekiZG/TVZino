70767afa017b9da705335c96c3eaf868372237d0
pekiZG <petar.slat@gmail.com>
2015-11-10 01:48:13 +0100

    Created upload page with dropzone.js
    Added new upload functionality.

--- searchApp/js/searchApp.js
+++ searchApp/js/searchApp.js
@@ -6,7 +6,7 @@ $(".form-control").keypress(function (event) {
         $.get("http://tvz.com.hr/api/search/", { board: $(".form-control").val() }, function (data) {
 
             $.each(data, function (index, attachment) {
-                $("tbody").append("<tr><td scope='row'>" + index + "</td><td><a href='http://tvz.com.hr/api/attachments/?id=" + attachment.ID_ATTACH + "'>" + attachment.filename + "</a></td></tr>");
+                $("tbody").append("<tr><th scope='row'>" + index + "</th><td><a href='http://tvz.com.hr/api/attachments/?id=" + attachment.ID_ATTACH + "'>" + attachment.filename + "</a></td></tr>");
             });
 
         }, "json");
